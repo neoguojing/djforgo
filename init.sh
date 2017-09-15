3cca9d40791ea6fa3cf38d953b269b19e714a16b
neoguojing <guojing_neo@163.com>
2017-09-15 19:41:26 +0800

    add init shell

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+go get github.com/alecthomas/log4go
+go get github.com/flosch/pongo2
+go get github.com/gorilla/context
+go get github.com/gorilla/mux
+go get github.com/gorilla/schema
+go get github.com/gorilla/sessions
+go get github.com/jinzhu/gorm
+go get github.com/jinzhu/gorm/dialects/mysql
+#go get github.com/prometheus/client_golang/prometheus
+#go get golang.org/x/crypto/bcrypt
