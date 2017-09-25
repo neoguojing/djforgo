f70530f150c0638f76775d52c754e67167c328d1
erjung <erjun.guo@jince.com>
2017-09-25 17:18:33 +0800

    update init

--- init.sh
+++ init.sh
@@ -9,5 +9,6 @@ go get github.com/gorilla/sessions
 go get github.com/jinzhu/gorm
 go get github.com/jinzhu/gorm/dialects/mysql
 go get github.com/deckarep/golang-set
+go get github.com/RangelReale/osin
 #go get github.com/prometheus/client_golang/prometheus
 #go get golang.org/x/crypto/bcrypt
